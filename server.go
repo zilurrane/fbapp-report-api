6e4ec572dd3a27aaf994614b2928c1652d207ec0
zilurrane <zilurrane@gmail.com>
2021-02-24 22:16:41 +0530

    Add Live Reload and Excel Report Output

--- server.go
+++ server.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/360EntSecGroup-Skylar/excelize/v2"
 	"github.com/gin-gonic/gin"
 	cors "github.com/rs/cors/wrapper/gin"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // Department Department
@@ -44,6 +52,21 @@ type FacultyComparionReportRequestBody struct {
 	Data []FacultyFeedback
 }
 
+// Feedback
+type Feedback struct {
+	FbNo           int       `json:"fbNo"`
+	DepartmentCode string    `json:"departmentCode"`
+	ClassCode      string    `json:"classCode"`
+	Faculty        string    `json:"faculty"`
+	CreatedDate    time.Time `json:"createdDate"`
+	Feedback       bson.M    `bson:"feedback"`
+}
+
+type FeedbackParameter struct {
+	ID   primitive.ObjectID `bson:"_id, omitempty"`
+	Code string             `json:"code"`
+}
+
 func main() {
 	r := gin.Default()
 
@@ -57,6 +80,7 @@ func main() {
 	r.Use(handler)
 	r.GET("/ping", healthStatusPing)
 	r.POST("/report/faculty/feedback/comparison", getFacultyFeedbackComparisonReport)
+	r.GET("/report/feedback", getFeedback)
 	var port string
 	if port = os.Getenv("PORT"); len(port) == 0 {
 		port = "8080"
@@ -68,6 +92,108 @@ func healthStatusPing(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
+}
+
+func toChar(i int) string {
+	return string('A' - 1 + i)
+}
+
+func getFeedback(c *gin.Context) {
+
+	tenantId := c.Request.Header.Get("TenantId")
+
+	clientOptions := options.Client().ApplyURI("mongodb+srv://adminbhau:adminbhau@cluster0.nokmu.mongodb.net")
+	client, err := mongo.NewClient(clientOptions)
+
+	//Set up a context required by mongo.Connect
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+
+	//To close the connection at the end
+	defer cancel()
+
+	err = client.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal("Couldn't connect to the database", err)
+	} else {
+		log.Println("Connected!")
+	}
+	db := client.Database("fbapp")
+
+	cursor, err := db.Collection("feedbacks").Find(context.TODO(), bson.M{})
+
+	if err != nil {
+		log.Printf("Error while getting all feedbacks, Reason: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  http.StatusInternalServerError,
+			"message": "Something went wrong",
+		})
+		return
+	}
+
+	findOptions := options.Find()
+
+	findOptions.SetSort(bson.D{{"createdDate", -1}})
+
+	parameterCursor, err := db.Collection("feedbackparameters").Find(context.TODO(), bson.M{"tenantId": bson.M{"$eq": tenantId}})
+
+	if err != nil {
+		log.Printf("Error while getting all feedback parameters, Reason: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  http.StatusInternalServerError,
+			"message": "Something went wrong",
+		})
+		return
+	}
+
+	var feedbackParameters []FeedbackParameter
+
+	for parameterCursor.Next(context.TODO()) {
+		var parameter FeedbackParameter
+		parameterCursor.Decode(&parameter)
+		feedbackParameters = append(feedbackParameters, parameter)
+	}
+
+	sheetName := "Feedback"
+	f := excelize.NewFile()
+
+	f.SetSheetName("Sheet1", sheetName)
+
+	f.SetCellValue(sheetName, fmt.Sprintf("B%d", 1), "Faculty")
+	f.SetCellValue(sheetName, fmt.Sprintf("C%d", 1), "Department")
+	f.SetCellValue(sheetName, fmt.Sprintf("D%d", 1), "Class")
+	f.SetCellValue(sheetName, fmt.Sprintf("E%d", 1), "CreatedAt")
+
+	for index, parameter := range feedbackParameters {
+		f.SetCellValue(sheetName, fmt.Sprintf("%s%d", toChar(index+6), 1), parameter.Code)
+	}
+
+	var feedbackIndex int = 1
+
+	for cursor.Next(context.TODO()) {
+		var feedback Feedback
+		cursor.Decode(&feedback)
+		feedbackIndex += 1
+		f.SetCellValue(sheetName, fmt.Sprintf("B%d", feedbackIndex), feedback.Faculty)
+		f.SetCellValue(sheetName, fmt.Sprintf("C%d", feedbackIndex), feedback.DepartmentCode)
+		f.SetCellValue(sheetName, fmt.Sprintf("D%d", feedbackIndex), feedback.ClassCode)
+		f.SetCellValue(sheetName, fmt.Sprintf("E%d", feedbackIndex), feedback.CreatedDate)
+
+		for index, parameter := range feedbackParameters {
+			f.SetCellValue(sheetName, fmt.Sprintf("%s%d", toChar(index+6), feedbackIndex), feedback.Feedback[parameter.ID.Hex()])
+		}
+	}
+
+	var b bytes.Buffer
+	if err := f.Write(&b); err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	downloadName := "Feedback" + ".xlsx"
+	c.Header("Content-Description", "File Transfer")
+	c.Header("Content-Disposition", "attachment; filename="+downloadName)
+	c.Data(http.StatusOK, "application/octet-stream", b.Bytes())
 }
 
 func getFacultyFeedbackComparisonReport(c *gin.Context) {
